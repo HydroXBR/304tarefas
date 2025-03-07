e583f8d12b2db5ef7b65d22f204f9dc77c2e978c
Psy <72590492+HydroXBR@users.noreply.github.com>
2025-03-07 03:32:55 -0400

    Update visits.js

--- database/visits.js
+++ database/visits.js
@@ -1,7 +1,7 @@
 import pkg from "mongoose"
 const {Schema, model} = pkg
 
-const visitSchema = new mongoose.Schema({
+const visitSchema = Schema({
     totalVisits: { type: Number, default: 0 },
     days: [{ type: String }] 
 });
